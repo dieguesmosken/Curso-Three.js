009ef2bed9309eb488ba1052e8e76bea2671ac5a
teuzin2022 <matheus.diegues@outlook.com>
2023-01-06 21:33:49 -0300

    pequenas mudanças

--- scenes/scene-01.js
+++ scenes/scene-01.js
@@ -1,17 +1,23 @@
 const geometry = new THREE.BoxBufferGeometry(1, 1, 1);
-const material = new THREE.MeshLambertMaterial(
-    { color: 0xffffff }
-);
+const material = new THREE.MeshLambertMaterial({ color: 0xffffff });
 
 const cube = new THREE.Mesh(
     geometry, material
 );
 
 scene.add(cube);
+x3.add(cube, { label: 'cube' });
 
 renderer.setAnimationLoop(() => {
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
 
-    renderer.render(scene, camera);
+
+    x3.tick();
+
+    x3.fps(() => {
+        renderer.render(scene, camera);
+    })
+
+
 });
